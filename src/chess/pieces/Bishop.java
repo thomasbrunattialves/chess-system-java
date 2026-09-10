9ee7981181ff8ecf1c55e07aad8706063f15a56a
Thomas Alves <thomasbrunattialves@gmail.com>
2026-09-10 19:41:17 +0100

    Bishop

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,9 +5,9 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Bishop extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Bishop(Board board, Color color) {
 		super(board, color);
 	}
 
@@ -18,48 +18,47 @@ public class Rook extends ChessPiece {
 
 		Position p = new Position(0, 0);
 
-		// above
-		p.setValues(position.getRow() - 1, position.getColumn());
+		// nw
+		p.setValues(position.getRow() - 1, position.getColumn() -1);
 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+			p.setValues(p.getRow() - 1 , p.getColumn() - 1 );
+		}
+		if (getBoard().positionExists(p) && isthereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// ne
+		p.setValues(position.getRow() -1, position.getColumn() + 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() -1, p.getColumn() + 1);
 
 		}
 		if (getBoard().positionExists(p) && isthereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// left
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		// se
+		p.setValues(position.getRow() +1 , position.getColumn() + 1);
 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 
 		}
 		if (getBoard().positionExists(p) && isthereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// right
-		p.setValues(position.getRow(), position.getColumn() + 1);
+		// sw
+		p.setValues(position.getRow() + 1 , position.getColumn() - 1);
 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);
-
-		}
-		if (getBoard().positionExists(p) && isthereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-
-		// below
-		p.setValues(position.getRow() +1 , position.getColumn());
-
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() +1);
+			p.setValues(p.getRow() +1, p.getColumn() -1);
 
 		}
 		if (getBoard().positionExists(p) && isthereOpponentPiece(p)) {
@@ -71,7 +70,7 @@ public class Rook extends ChessPiece {
 	
 	@Override
 	public String toString() {
-		return "R";
+		return "B";
 	}
 
 
